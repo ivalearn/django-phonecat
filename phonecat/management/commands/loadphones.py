13c7631c0d72c3908163ac3b3509b868a4ce4647
ivandeex <ivandeex@gmail.com>
2016-07-28 15:14:27 +0300

    auto-checkout angular project for media import

--- phonecat/management/commands/loadphones.py
+++ phonecat/management/commands/loadphones.py
@@ -10,21 +10,25 @@ class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
 
     def add_arguments(self, parser):
-        parser.add_argument('phonecat_app_dir', nargs=1)
+        parser.add_argument('phonecat_dir_or_github', nargs=1)
 
     def handle(self, *args, **options):
-        phonecat_app_dir = options['phonecat_app_dir'][0]
-        with open(os.path.join(phonecat_app_dir, 'phones', 'phones.json')) as f:
+        phonecat_dir = options['phonecat_dir_or_github'][0]
+        if phonecat_dir == 'github':
+            phonecat_dir = os.path.join(settings.TEMP_DIR, 'angular-phonecat-git')
+            phonecat_url = 'https://github.com/angular/angular-phonecat.git'
+            os.system('git clone --depth=16 -b step-14 %s %s' % (phonecat_url, phonecat_dir))
+        with open(os.path.join(phonecat_dir, 'app', 'phones', 'phones.json')) as f:
             all_data = json.load(f)
         phones = []
         for pk, phone in enumerate(all_data, start=1):
-            with open(os.path.join(phonecat_app_dir, 'phones', phone['id'] + '.json')) as f:
+            with open(os.path.join(phonecat_dir, 'app', 'phones', phone['id'] + '.json')) as f:
                 props = json.load(f)
                 props.pop('id')
                 kwargs = dict(name=props.pop('name'), age=phone['age'], snippet=phone['snippet'])
                 MAX_IMAGES = 5
                 for idx, filepath in enumerate(props.pop('images', [])[:MAX_IMAGES], start=1):
-                    src_path = os.path.join(phonecat_app_dir, filepath)
+                    src_path = os.path.join(phonecat_dir, 'app', filepath)
                     filename = os.path.basename(filepath)
                     subdir = Phone._meta.get_field('image%d' % idx).upload_to
                     kwargs['image%d' % idx] = os.path.join(subdir, filename)
